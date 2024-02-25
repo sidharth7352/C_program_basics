71bade222f46a9339578a8c613636e3f750a79c9
Sidharth K <143076733+sidharth7352@users.noreply.github.com>
2024-02-25 10:40:56 +0530

    deleting last digit

---
+++ lst_dgt_dlt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int num,r;
+printf("Enter the number...\n");
+scanf("%d",&num);
+printf("before last number deletion num=%d\n",num);
+num=num/10;
+printf("After last number deletion num=%d\n",num);
+}
